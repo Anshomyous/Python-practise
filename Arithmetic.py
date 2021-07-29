cec9ddb83436faad41b46baffdf836d7b5118166
Soham Ruj <sohamr@infinite.com>
2021-07-30 01:14:44 +0530

    Programs added

---
+++ Arithmetic.py
@@ -0,0 +1,8 @@
+x = 3
+y = 4
+z = x + y
+
+print(z)
+
+name = 'youtube'
+print(name[-1])
---
+++ Build.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("mvn clean install D:\Research Work\Server Work\PRIME-DEV\rpm-frontend")
